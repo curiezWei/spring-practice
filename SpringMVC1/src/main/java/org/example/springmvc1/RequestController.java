6ce92e628123a559d5f652c07569ef7dd56d1d20
curiez <3344366756@qq.com>
2026-03-17 13:08:25 +0800

    @RequestParam

--- SpringMVC1/src/main/java/org/example/springmvc1/RequestController.java
+++ SpringMVC1/src/main/java/org/example/springmvc1/RequestController.java
@@ -1,6 +1,7 @@
 package org.example.springmvc1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/request")
 @RestController
@@ -25,4 +26,22 @@ public class RequestController {
         return "age is " + age;
     }
 
+    //传对象
+    @RequestMapping("/r4")
+    public String r4(Person person) {
+        return "personal information: "+ person;
+    }
+
+    //参数取别名，前端传name，后端用userName
+    //强制传
+    @RequestMapping("/r5")
+    public String r5(@RequestParam("name")String userName) {
+        return "name is "+userName;
+    }
+
+    //非强制传
+    @RequestMapping("/r6")
+    public String r6(@RequestParam(value = "name",required = false)String userName) {
+        return "name is "+userName;
+    }
 }
