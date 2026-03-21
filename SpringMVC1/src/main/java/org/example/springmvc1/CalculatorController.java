0faab88105edacbd1e907ab12a5eeb71fd622881
curiez <3344366756@qq.com>
2026-03-22 00:52:50 +0800

    计算器🧮

---
+++ SpringMVC1/src/main/java/org/example/springmvc1/CalculatorController.java
@@ -0,0 +1,14 @@
+package org.example.springmvc1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cal")
+public class CalculatorController {
+    @RequestMapping("/sum")
+    public String sum(Integer num1,Integer num2){
+        Integer sum = num1+num2;
+        return "和为："+sum;
+    }
+}
--- SpringMVC1/src/main/java/org/example/springmvc1/SessionController.java
+++ SpringMVC1/src/main/java/org/example/springmvc1/SessionController.java
@@ -4,9 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -41,5 +39,27 @@ public class SessionController {
         HttpSession session = request.getSession();
         String name = (String) session.getAttribute("name");
         return "session 中的 name："+name;
+    }
+
+    @RequestMapping("/getSession2")
+    public String getSession2(HttpSession session) {
+        String name = (String)session.getAttribute("name");
+        return "session中的name"+" "+name;
+    }
+
+    @RequestMapping("/getSession3")
+    public String getSession3(@SessionAttribute("name") String name) {
+        return "session中的name"+" "+name;
+    }
+
+    @RequestMapping("/getHeader")
+    public String getHeader(HttpServletRequest request){
+        String userAgent = request.getHeader("User-Agent");
+        return userAgent;
+    }
+
+    @RequestMapping("/getHeader2")
+    public String getHeader(@RequestHeader("User-Agent") String userAgent){
+        return userAgent;
     }
 }
