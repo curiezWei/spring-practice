cdd799723f8126f3cb7d313ae8842dd58382f6be
curiez <3344366756@qq.com>
2026-03-31 23:53:37 +0800

    五大注解

---
+++ SpringIOC-DI/src/main/java/org/example/springiocdi/Controller/UserController.java
@@ -0,0 +1,10 @@
+package org.example.springiocdi.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    public void method() {
+        System.out.println("User Controller....");
+    }
+}
