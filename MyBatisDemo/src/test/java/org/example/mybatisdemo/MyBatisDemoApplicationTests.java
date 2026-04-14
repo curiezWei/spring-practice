13fa42ab1e77e66a2800aed473192a1f84c5c20c
curiez <3344366756@qq.com>
2026-04-14 11:55:54 +0800

    mybatis config配置

---
+++ MyBatisDemo/src/test/java/org/example/mybatisdemo/MyBatisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
